dfabb4b87001917b4d7dae078d9e0f896e927289
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-04-04 15:59:37 +0800

    review this part

--- script.js
+++ script.js
@@ -101,3 +101,42 @@
 // const f = jonas.calcAge;
 
 // f();
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // const self = this;
+    // const isMillenial = function () {
+    //   //   console.log(this.year >= 981 && this.year <= 1996);
+    //   console.log(self.year >= 981 && self.year <= 1996);
+    // };
+
+    const isMillenial = () => {
+      //   console.log(this.year >= 981 && this.year <= 1996);
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+jonas.greet();
+jonas.calcAge();
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 9, 0, 10);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 7);
